79dc123c530f1a31826d9b4641a985a125508b62
songjiadong@miic.com.cn <songjiadong@miic.com.cn>
2023-08-21 13:20:12 +0800

    优化代码

--- aries.service/Controllers/GalaxyController.query.cs
+++ aries.service/Controllers/GalaxyController.query.cs
@@ -12,88 +12,28 @@ namespace aries.service.Controllers
         [HttpPost("graph")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult> Graph(GraphDegreeReq graphReq)
+        public ActionResult Graph(GraphDegreeReq graphReq)
         {
             ActionResult result;
             AriesGalaxyGrpc.GraphDegreeReq req = graphReq.Convert();
-            try
+            result= Get<GalaxyController, AriesJsonObjResp>(async () =>
             {
-                var temp = await client.InvokeMethodGrpcAsync<AriesGalaxyGrpc.GraphDegreeReq, AriesJsonObjResp>(daprappqueryId, "Galaxy$Query$Graph", req);
-                result = Ok(temp);
-            }
-            catch (DaprApiException ex)
-            {
-                result = BadRequest(ex);
-                LoggerService.Logger<GalaxyController>(ex, LogLevel.Error);
-            }
-            catch (DaprException ex)
-            {
-                result = BadRequest(ex);
-                LoggerService.Logger<GalaxyController>(ex, LogLevel.Error);
-            }
-            catch (Exception ex)
-            {
-                result = BadRequest(ex);
-                LoggerService.Logger<GalaxyController>(ex, LogLevel.Error);
-            }
+              return  await client.InvokeMethodGrpcAsync<AriesGalaxyGrpc.GraphDegreeReq, AriesJsonObjResp>(daprappqueryId, "Galaxy$Query$Graph", req);
+            });
             return result;
         }
-        //[HttpPost("autocomplete")]
-        //[ProducesResponseType(StatusCodes.Status200OK)]
-        //[ProducesResponseType(StatusCodes.Status400BadRequest)]
-        //public async Task<ActionResult> AutoComplete(GraphSearchReq searchReq)
-        //{
-        //    ActionResult result;
-        //    try
-        //    {
-        //        var temp = await client.InvokeMethodGrpcAsync<GraphSearchReq, AriesJsonObjResp>(daprappqueryId, "Galaxy$Query$AutoComplete", searchReq);
-        //        result = Ok(temp);
-        //    }
-        //    catch (DaprApiException ex)
-        //    {
-        //        result = BadRequest(ex);
-        //        LoggerService.Logger<GalaxyController>(ex, LogLevel.Error);
-        //    }
-        //    catch (DaprException ex)
-        //    {
-        //        result = BadRequest(ex);
-        //        LoggerService.Logger<GalaxyController>(ex, LogLevel.Error);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        result = BadRequest(ex);
-        //        LoggerService.Logger<GalaxyController>(ex, LogLevel.Error);
-        //    }
-        //    return result;
-        //}
+       
         [HttpPost("shortestpath")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult> ShortestPath(ShortestPathReq searchReq)
+        public ActionResult ShortestPath(ShortestPathReq shortestPathReq)
         {
             ActionResult result;
-            AriesGalaxyGrpc.ShortestPathReq req = searchReq.Convert();
-            try
+            AriesGalaxyGrpc.ShortestPathReq req = shortestPathReq.Convert();
+            result = Get<GalaxyController, AriesJsonObjResp>(async () =>
             {
-                var temp = await client.InvokeMethodGrpcAsync<AriesGalaxyGrpc.ShortestPathReq, AriesJsonObjResp>(daprappqueryId, "Galaxy$Query$ShortestPath", req);
-                result = Ok(temp);
-            }
-            catch (DaprApiException ex)
-            {
-                result = BadRequest(ex);
-                LoggerService.Logger<GalaxyController>(ex, LogLevel.Error);
-            }
-            catch (DaprException ex)
-            {
-                result = BadRequest(ex);
-                LoggerService.Logger<GalaxyController>(ex, LogLevel.Error);
-
-            }
-            catch (Exception ex)
-            {
-                result = BadRequest(ex);
-                LoggerService.Logger<GalaxyController>(ex, LogLevel.Error);
-            }
+              return  await client.InvokeMethodGrpcAsync<AriesGalaxyGrpc.ShortestPathReq, AriesJsonObjResp>(daprappqueryId, "Galaxy$Query$ShortestPath", req);
+            });  
             return result;
         }
         
