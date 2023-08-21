f80465debdb2a4fcf0c3afdf19f1a020f4f3b592
songjiadong@miic.com.cn <songjiadong@miic.com.cn>
2023-08-21 15:53:27 +0800

    修正图数据相关

---
+++ aries.service/Views/request/galaxy/PersonInfoReq.cs
@@ -0,0 +1,22 @@
+﻿using aries.webapi;
+using AriesGrpc = aries.galaxy.grpc;
+
+namespace aries.service.galaxy.Views.request
+{
+    public class PersonInfoReq : TReq<AriesGrpc.PersonInfoReq>
+    {
+        public PersonInfoReq() { }
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public override AriesGrpc.PersonInfoReq Convert()
+        {
+            return new AriesGrpc.PersonInfoReq
+            {
+                 Person=new AriesGrpc.PersonInfo() 
+                 {
+
+                 }
+            };
+        }
+    }
+}
