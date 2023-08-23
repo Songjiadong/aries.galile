22d0a46fc0c9fde56852647b43d312eb4c991418
songjiadong@miic.com.cn <songjiadong@miic.com.cn>
2023-08-23 12:23:38 +0800

    添加荣誉相关的实体类

---
+++ aries.service/Views/request/galaxy/HonorInfoReq.cs
@@ -0,0 +1,23 @@
+﻿using aries.galaxy.grpc;
+using aries.webapi;
+using AriesGrpc = aries.galaxy.grpc;
+namespace aries.service.galaxy.Views.request
+{
+    public class HonorInfoReq : TReq<AriesGrpc.HonorInfoReq>
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+
+        public override AriesGrpc.HonorInfoReq Convert()
+        {
+            return new AriesGrpc.HonorInfoReq()
+            {
+                Honor = new AriesGrpc.HonorInfo()
+                {
+                    Id = Id,
+                    Name = Name,
+                }
+            };
+        }
+    }
+}
