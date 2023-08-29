a9f01f062e32508c4bc24acc3ef8e250854cfb23
songjiadong@miic.com.cn <songjiadong@miic.com.cn>
2023-08-29 12:14:37 +0800

    修改gethotlist

--- aries.galaxy.command/Enums/InfoStatusEnum.cs
+++ aries.galaxy.command/Enums/InfoStatusEnum.cs
@@ -6,9 +6,11 @@ namespace aries.galaxy.command
     {
         [Description("初始化")]
         Init = 0,
+        [Description("已导入")]
+        Load=1,
         [Description("已编辑")]
-        Edited = 1,
+        Edited = 2,
         [Description("可提交")]
-        CanSubmit =2
+        CanSubmit =3
     }
 }
